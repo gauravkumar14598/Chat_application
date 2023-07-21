8e70db183ee067df62ccc75e2657ea5fb627b50b
gauravkumar14598 <gauravkulhari818@gmail.com>
2023-07-21 10:34:32 +0530

    photos added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:chat_application/screens/auth_screen.dart';
 import 'package:chat_application/screens/chat_screen.dart';
 import 'package:chat_application/screens/splash.dart';
@@ -5,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -22,18 +23,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (ctx, userSnapshot) {
-          if (userSnapshot.connectionState == ConnectionState.waiting) {
-            return const SplashScreen();
-          }
-          if (userSnapshot.hasData) {
-            return const ChatScreen();
-          }
-          return const AuthScreen();
-        },
-      ),
+
+      home:  StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (ctx, userSnapshot){
+        if(userSnapshot.connectionState==ConnectionState.waiting){
+          return const SplashScreen();
+        }
+        if(userSnapshot.hasData){
+          return const ChatScreen();
+        }
+        return const AuthScreen();
+      },),
     );
   }
 }
